64874fd34c973b2dc8c464611b46ce0440c7b69d
yogajones <95644662+yogajones@users.noreply.github.com>
2024-02-01 15:13:28 +0200

    Ex 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
 
+const FeedbackButton = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
+  return (<>
+    <h1>Statistics</h1>
+    <p>Good: {good}</p>
+    <p>Neutral: {neutral}</p>
+    <p>Bad: {bad}</p>
+  </>)
+}
+
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
-      code here
+      <h1>Unicafe feedback</h1>
+      <FeedbackButton handleClick={() => setGood(good + 1)} text={"GOOD"} />
+      <FeedbackButton handleClick={() => setNeutral(neutral + 1)} text={"NEUTRAL"} />
+      <FeedbackButton handleClick={() => setBad(bad + 1)} text={"BAD"} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
