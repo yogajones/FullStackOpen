677773c0a8bead75978650b9813428e5219a5730
yogajones <95644662+yogajones@users.noreply.github.com>
2024-02-02 13:26:38 +0200

    Exs 2.2, 2.3

--- part2/kurssitiedot/src/components/Course.jsx
+++ part2/kurssitiedot/src/components/Course.jsx
@@ -1,28 +1,30 @@
-const Header = ({ name }) => {
-    return (<h1>{name}</h1>)
-}
+const Header = ({ name }) => (<h1>{name}</h1>)
 
-const Part = ({ part }) => {
-    return (
-        <p key={part.id}>{part.name} {part.exercises}</p>
-    )
-}
+const Part = ({ part }) => (
+    <p key={part.id}>{part.name} {part.exercises}</p>
+)
 
-const Content = ({ parts }) => {
+const Content = ({ parts }) => (
+    <div>
+        {parts.map(part => <Part key={part.id} part={part} />)}
+    </div>
+)
+
+const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => { return sum + part.exercises }, 0)
     return (
         <div>
-            {parts.map(part => <Part key={part.id} part={part} />)}
+            <p>Total of {total} exercises</p>
         </div>
     )
 }
 
-const Course = ({ course }) => {
-    return (
-        <div>
-            <Header name={course.name} />
-            <Content parts={course.parts} />
-        </div>
-    )
-}
+const Course = ({ course }) => (
+    <div>
+        <Header name={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+    </div>
+)
 
 export default Course
