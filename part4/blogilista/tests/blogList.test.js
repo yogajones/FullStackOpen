a3aa21b0466378226ca294d797cb973c7195ee77
yogajones <95644662+yogajones@users.noreply.github.com>
2024-03-04 16:28:46 +0200

    Ex 4.5

--- part4/blogilista/tests/blogList.test.js
+++ part4/blogilista/tests/blogList.test.js
@@ -2,6 +2,41 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+const oneBlog = {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0
+}
+
+const threeBlogs = [{
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0
+},
+{
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+},
+{
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
+}]
+
+
 test('dummy returns one', () => {
     const blogs = []
 
@@ -14,41 +49,21 @@ describe('total likes', () => {
         assert.strictEqual(listHelper.totalLikes([]), 0)
     })
     test('of one blog equals its likes', () => {
-        assert.strictEqual(listHelper.totalLikes([{
-            _id: '5a422a851b54a676234d17f7',
-            title: 'React patterns',
-            author: 'Michael Chan',
-            url: 'https://reactpatterns.com/',
-            likes: 7,
-            __v: 0
-        }]), 7)
+        assert.strictEqual(listHelper.totalLikes([oneBlog]), oneBlog.likes)
     })
     test('of three blogs equals the sum of likes', () => {
-        assert.strictEqual(listHelper.totalLikes([
-            {
-                _id: '5a422a851b54a676234d17f7',
-                title: 'React patterns',
-                author: 'Michael Chan',
-                url: 'https://reactpatterns.com/',
-                likes: 7,
-                __v: 0
-            },
-            {
-                _id: '5a422aa71b54a676234d17f8',
-                title: 'Go To Statement Considered Harmful',
-                author: 'Edsger W. Dijkstra',
-                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-                likes: 5,
-                __v: 0
-            },
-            {
-                _id: '5a422b3a1b54a676234d17f9',
-                title: 'Canonical string reduction',
-                author: 'Edsger W. Dijkstra',
-                url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-                likes: 12,
-                __v: 0
-            }
-        ]), 24)
+        assert.strictEqual(listHelper.totalLikes(threeBlogs), 24)
+    })
+})
+
+describe('favorite blog', () => {
+    test('of an empty list is an error message', () => {
+        assert.strictEqual(listHelper.favoriteBlog([]), 'No favorite!')
+    })
+    test('of a list with one blog is that blog', () => {
+        assert.strictEqual(listHelper.favoriteBlog([oneBlog]), oneBlog)
+    })
+    test('of a list of blogs is the one with the most likes', () => {
+        assert.strictEqual(listHelper.favoriteBlog(threeBlogs), threeBlogs[2])
     })
 })
--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -10,7 +10,16 @@ const totalLikes = (blogs) => {
         ? 0 
         : blogs.reduce(likesReducer, 0)
 }
-  
+
+const favoriteBlog = (blogs) => {
+    const favoriteReducer = (favorite, current) => {
+        return (current.likes >= favorite.likes) ? current : favorite
+    }
+    return blogs.length === 0
+        ? 'No favorite!'
+        : blogs.reduce(favoriteReducer, {likes: 0})
+}
+
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
 }
