9a4c61e0e566b370215c300b342afd53ab3dba1a
yogajones <95644662+yogajones@users.noreply.github.com>
2024-03-11 12:32:12 +0200

    Ex 4.17

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,22 +1,37 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({})
+        .populate('user', { username: 1, name: 1, id: 1})
+
     response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
+    //const user = await User.findById(body.userId)
+
+    // begin temp
+    const users = await User.find({})
+    const user = users[0]
+    console.log(user)
+    // end temp
 
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes
+        likes: body.likes,
+        user: user._id
     })
 
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog.id)
+    await user.save()
+
     response.status(201).json(savedBlog)
 })
 
