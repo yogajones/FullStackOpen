4a446e45f6fa2303377618b691cf08db28d921da
yogajones <95644662+yogajones@users.noreply.github.com>
2024-03-27 17:44:54 +0200

    Ex 5.15

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -3,14 +3,15 @@ import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('Renders blog:', () => {
+  const mockUser = { id: '999999999999999999999999991' }
   const blog = {
     title: 'Title',
     author: 'Author',
     url: 'www',
+    user: mockUser
   }
   const mockLike = vi.fn()
   const mockDelete = vi.fn()
-  const mockUser = {}
 
   test('renders only title and author by default', () => {
     const { container } = render(<Blog blog={blog} likeBlog={mockLike} deleteBlog={mockDelete} user={mockUser} />)
@@ -36,5 +37,29 @@ describe('Renders blog:', () => {
     const button = screen.getByText('view')
     await user.click(button)
     screen.getByText('hide')
+  })
+})
+
+describe('Calls event handlers:', () => {
+  const mockUser = { id: '999999999999999999999999991' }
+
+  const blog = {
+    title: 'Title',
+    author: 'Author',
+    url: 'www',
+    user: mockUser
+  }
+  const mockLike = vi.fn()
+  const mockDelete = vi.fn()
+
+  test('calls like function twice after clicking the like button twice', async () => {
+    render(<Blog blog={blog} likeBlog={mockLike} deleteBlog={mockDelete} user={mockUser} />)
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+    expect(mockLike.mock.calls).toHaveLength(2)
   })
 })
