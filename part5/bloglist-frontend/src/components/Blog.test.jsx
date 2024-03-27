3005d222250812e29d127b423dc904c991fc0f06
yogajones <95644662+yogajones@users.noreply.github.com>
2024-03-27 17:31:28 +0200

    Ex 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,24 +1,40 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders only title and author before clicking view', () => {
+describe('Renders blog:', () => {
   const blog = {
     title: 'Title',
     author: 'Author',
     url: 'www',
   }
-
   const mockLike = vi.fn()
   const mockDelete = vi.fn()
   const mockUser = {}
 
-  const { container } = render(<Blog blog={blog} likeBlog={mockLike} deleteBlog={mockDelete} user={mockUser} />)
+  test('renders only title and author by default', () => {
+    const { container } = render(<Blog blog={blog} likeBlog={mockLike} deleteBlog={mockDelete} user={mockUser} />)
 
-  const hideDetails = container.querySelector('#hide-details')
-  screen.debug(hideDetails)
+    const hideDetails = container.querySelector('#hide-details')
+    expect(hideDetails.textContent).toContain(blog.title)
+    expect(hideDetails.textContent).toContain(blog.author)
+    expect(hideDetails.textContent).not.toContain(blog.url)
+    expect(hideDetails.textContent).not.toContain('Likes:')
+  })
+  test('renders also url, likes and user after clicking view', () => {
+    const { container } = render(<Blog blog={blog} likeBlog={mockLike} deleteBlog={mockDelete} user={mockUser} />)
 
-  expect(hideDetails.textContent).toContain(blog.title)
-  expect(hideDetails.textContent).toContain(blog.author)
-  expect(hideDetails.textContent).not.toContain(blog.url)
-  expect(hideDetails.textContent).not.toContain('Likes:')
+    const showDetails = container.querySelector('#show-details')
+    expect(showDetails.textContent).toContain(blog.title)
+    expect(showDetails.textContent).toContain(blog.author)
+    expect(showDetails.textContent).toContain(blog.url)
+    expect(showDetails.textContent).toContain('Likes:')
+  })
+  test('shows hide button after clicking view', async () => {
+    render(<Blog blog={blog} likeBlog={mockLike} deleteBlog={mockDelete} user={mockUser} />)
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+    screen.getByText('hide')
+  })
 })
