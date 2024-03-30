83fc6c01b1fed0be82f308b2ade7d084f6308dd0
yogajones <95644662+yogajones@users.noreply.github.com>
2024-03-30 15:33:54 +0200

    Ex 5.18

--- part5/bloglist-e2e/tests/bloglist_app.spec.js
+++ part5/bloglist-e2e/tests/bloglist_app.spec.js
@@ -24,4 +24,23 @@ describe('Bloglist app', () => {
 
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
   })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.locator('input[name="Username"]').fill('teepee')
+      await page.locator('input[name="Password"]').fill('restful')
+      await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('Logged in as teepee')).toBeVisible()
+      await expect(page.getByRole('heading', { name: 'All blogs' })).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.locator('input[name="Username"]').fill('teepee')
+      await page.locator('input[name="Password"]').fill('restless')
+      await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('Logged in as teepee')).not.toBeVisible()
+      await expect(page.getByRole('heading', { name: 'All blogs' })).not.toBeVisible()
+      await expect(page.getByText('Wrong username or password')).toBeVisible()
+    })
+  })
 })
