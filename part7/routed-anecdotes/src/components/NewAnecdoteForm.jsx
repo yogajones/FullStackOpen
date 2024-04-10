5c9d100315401019c7209433e5b361ac9bb36d8f
yogajones <95644662+yogajones@users.noreply.github.com>
2024-04-10 16:00:07 +0300

    Ex 7.4

--- part7/routed-anecdotes/src/components/NewAnecdoteForm.jsx
+++ part7/routed-anecdotes/src/components/NewAnecdoteForm.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
 import PropTypes from 'prop-types'
 import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks'
 
 
 const NewAnecdoteForm = ({ addNew }) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
+    const content = useField('text')
+    const author = useField('text')
+    const info = useField('text')
+
     const navigate = useNavigate()
 
     const handleSubmit = (e) => {
         e.preventDefault()
         addNew({
-            content,
-            author,
-            info,
+            content: content.value,
+            author: author.value,
+            info: info.value,
             votes: 0
         })
         navigate('/')
@@ -25,16 +26,16 @@ const NewAnecdoteForm = ({ addNew }) => {
             <h2>create a new anecdote</h2>
             <form onSubmit={handleSubmit}>
                 <div>
-                    content
-                    <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+                    content:
+                    <input {...content} />
                 </div>
                 <div>
-                    author
-                    <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+                    author:
+                    <input {...author} />
                 </div>
                 <div>
-                    url for more info
-                    <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+                    url for more info:
+                    <input {...info} />
                 </div>
                 <button>create</button>
             </form>
